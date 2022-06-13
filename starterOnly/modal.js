fe8a2e940d2aea92b09283e3233344c6c5d6dfcd
Mehdony <86969900+Mehdony@users.noreply.github.com>
2022-06-13 19:13:19 +0200

    Verification des datas radios + debut récuperation des saisies input

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,8 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
+const btnSub = document.querySelector(".btn-submit");
+console.log(btnSub);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -21,13 +23,41 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-// close modal form
+// Close modal form
 
 function closemodal() {
   modalbg.style.display = "none";
 }
-  // Close modal form 
-closeBtn.addEventListener("click", closemodal); 
-  
+closeBtn.addEventListener("click", closemodal);
 
-  
+
+
+//verification des datas radio
+const isChecked = (location) => {
+  for (i = 0; i < location.length; i++) {
+    if (location[i].checked === true) {
+      console.log(true);
+      return true
+    }
+    return false;
+  }
+}
+// récupération du nom 
+const isNameValid = (name) => {
+  if (name.length > 2) {
+      return true
+    }
+    return false;
+  }
+
+
+
+// récupération des données du formulaire
+btnSub.addEventListener("click", (e) => {
+  e.preventDefault();
+  let location = document.querySelectorAll("input[name = 'location']");
+})
+
+// stockage des saisies dans le LS
+// localStorage.setitem("first", document.querySelector("#first").value);
+// console.log(document.querySelector("#first").value);
